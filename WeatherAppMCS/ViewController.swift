b98752b544b5ac01fa1b4c57d222e97a6692b917
MCS <mcs@MCSs-MacBook-Pro.local>
2017-11-07 13:22:20 -0500

    Initial Commit

---
+++ WeatherAppMCS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherAppMCS
+//
+//  Created by MCS on 11/7/17.
+//  Copyright © 2017 Zach Chandler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
